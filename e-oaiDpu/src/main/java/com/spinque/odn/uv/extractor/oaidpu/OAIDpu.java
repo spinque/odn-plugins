ce342dbf439e622f75c7905907f5617b8fc2c73d
Wouter Alink <wouter@spinque.com>
2015-07-02 11:53:10 +0200

    -- bugfix: use correct outputter

--- e-oaiDpu/src/main/java/com/spinque/odn/uv/extractor/oaidpu/OAIDpu.java
+++ e-oaiDpu/src/main/java/com/spinque/odn/uv/extractor/oaidpu/OAIDpu.java
@@ -20,14 +20,11 @@ import com.spinque.utils.Utils;
 import eu.unifiedviews.dataunit.DataUnit;
 import eu.unifiedviews.dataunit.DataUnitException;
 import eu.unifiedviews.dataunit.files.WritableFilesDataUnit;
-import eu.unifiedviews.dataunit.rdf.WritableRDFDataUnit;
 import eu.unifiedviews.dpu.DPU;
 import eu.unifiedviews.dpu.DPUException;
 import eu.unifiedviews.helpers.dpu.config.ConfigHistory;
 import eu.unifiedviews.helpers.dpu.context.ContextUtils;
 import eu.unifiedviews.helpers.dpu.exec.AbstractDpu;
-import eu.unifiedviews.helpers.dpu.extension.ExtensionInitializer;
-import eu.unifiedviews.helpers.dpu.extension.faulttolerance.FaultTolerance;
 
 /**
  * Main data processing unit class.
@@ -39,15 +36,9 @@ public class OAIDpu extends AbstractDpu<OAIDpuConfig_V1> implements DPU {
 
     private static final Logger LOG = LoggerFactory.getLogger(OAIDpu.class);
 		
-    @ExtensionInitializer.Init
-    public FaultTolerance faultTolerance;
-    
     @DataUnit.AsOutput(name = "output")
     public WritableFilesDataUnit output;
     
-    @DataUnit.AsOutput(name = "outputRDF")
-    public WritableRDFDataUnit outputRDF;
-
 	public OAIDpu() {
 		super(OAIDpuVaadinDialog.class, ConfigHistory.noHistory(OAIDpuConfig_V1.class));
 	}
@@ -59,7 +50,7 @@ public class OAIDpu extends AbstractDpu<OAIDpuConfig_V1> implements DPU {
         
         LOG.info("Configuration: " + config.toString());
         try {
-        	RepositoryConnection rc = outputRDF.getConnection();
+        	RepositoryConnection rc = output.getConnection();
         	try {
 
         		/* crawl the OAI repository */
