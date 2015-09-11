688e65546ae5e5e6f946a6f73cab37ae20b6fc00
Roberto Cornacchia <roberto@spinque.com>
2015-09-11 09:47:59 +0200

    commit leftovers

--- e-oaiDpu/src/main/java/com/spinque/utils/ClosableIterator.java
+++
@@ -1,8 +0,0 @@
-package com.spinque.utils;
-
-import java.io.IOException;
-import java.util.Iterator;
-
-public interface ClosableIterator<T> extends Iterator<T> {
-	void close() throws IOException;
-}
